346d441e33835e398db252368209d50bbc800786
Lee-Way Wang <lee.way56@comcast.net>
2021-07-08 18:49:39 -0700

    Begin work on constructor & several methods

--- LetterInventory.R
+++ LetterInventory.R
@@ -1,6 +1,54 @@
 # 
 
-LetterInventory <- function(){
-  value <- list()
-  return(value)
+
+calculate_inventory <- function(str){
+  inventory <- c()
+  return(inventory)
 }
+
+
+LetterInventory <- setClass("LetterInventory", slots = list(
+                                                            inventory = calculate_inventory,
+                                                            size = length(str)))
+
+
+setGeneric("get", function(LetterInventory) {
+  standardGeneric("get")
+})
+
+
+setMethod("get", signature(object = "LetterInventory"), 
+          function(LetterInventory){
+          })
+
+
+setGeneric("size", function(LetterInventory) {
+  standardGeneric("size")
+})
+
+
+setMethod("size", signature(object = "LetterInventory"), 
+          function(LetterInventory){
+            return(LetterInventory@size)
+          })
+
+
+setGeneric("isEmpty", function(LetterInventory) {
+  standardGeneric("isEmpty")
+})
+
+
+setMethod("isEmpty", signature(object = "LetterInventory"), 
+          function(LetterInventory){
+            return(size == 0)
+          })
+
+
+setGeneric("toString", function(LetterInventory) {
+  standardGeneric("toString")
+})
+
+
+setMethod("toString", signature(object = "LetterInventory"), 
+          function(LetterInventory){
+          })
